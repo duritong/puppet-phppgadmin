8188c65b9478751bf4ec4ac62e5cf2d86268b496
andreas <andreas@immerda.ch>
2008-02-29 18:14:28 +0000

    * verschieben * phpgpadmin und phpmyadmin erweitern

--- manifests/init.pp
+++ manifests/init.pp
@@ -1,9 +1,34 @@
-# modules/skeleton/manifests/init.pp - manage skeleton stuff
+# modules/phppgadmin/manifests/init.pp - manage phppgadmin stuff
 # Copyright (C) 2007 admin@immerda.ch
 #
 
-# modules_dir { "skeleton": }
+# modules_dir { "phppgadmin": }
 
-class skeleton {
+class phppgadmin {
 
+    case $operatingsystem {
+        gentoo: { include webapp-config }
+    }
+    
+    package { 'phppgadmin':
+        ensure => present,
+        category => $operatingsystem ? {
+            gentoo => 'dev-db',
+            default => '',
+        }
+    }
+
+    # config files
+    file{
+        "/var/www/localhost/htdocs/phppgadmin/conf/config.inc.php":
+            source => [
+                "puppet://$server/dist/phppgadmin/${fqdn}/config.inc.php",
+                "puppet://$server/phppgadmin/${fqdn}/config.inc.php",
+                "puppet://$server/phppgadmin/config.inc.php"
+            ],
+            owner => root,
+            group => 0,
+            mode => 0444,
+            require => Package[phppgadmin],
+    }
 }
